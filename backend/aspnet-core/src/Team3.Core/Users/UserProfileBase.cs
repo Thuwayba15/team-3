d2afac0df75055bc60f0bb82d956c06e966c1cbe
RRusso15 <166402938+RRusso15@users.noreply.github.com>
2026-03-16 23:39:13 +0200

    Setup User Profile classes
    All the user profile classes were implemented

---
+++ backend/aspnet-core/src/Team3.Core/Users/UserProfileBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team3.Users
+{
+    internal class UserProfileBase
+    {
+    }
+}
